24a4aa11ff20a62bb0b626820ac49bad369cf3bc
Brieuc Fresnel <brieuc@studio-dot.fr>
2022-06-10 11:17:36 +0200

    Add \Main namespace part to classes

--- includes/Admin/Components/ComponentsSingle.php
+++ includes/Admin/Components/ComponentsSingle.php
@@ -25,12 +25,11 @@ class ComponentsSingle {
 
     public function load_single() {
         add_action('add_meta_boxes', array($this, 'metaboxes'));
-
     }
 
     public function add_edit_instructions($post) {
         global $typenow;
-        if ($typenow !== \DOT\Core\Components::$post_type)
+        if ($typenow !== \DOT\Core\Main\Components::$post_type)
             return;
 
         $field_group_id = false;
@@ -39,7 +38,7 @@ class ComponentsSingle {
         foreach ($field_groups as $field_group) {
             // Get first location rule only
             $location = $field_group['location'][0][0];
-            if ($location['param'] === \DOT\Core\Components::$post_type && $location['value'] == $post->ID) {
+            if ($location['param'] === \DOT\Core\Main\Components::$post_type && $location['value'] == $post->ID) {
                 $field_group_id = $field_group['ID'];
             }
         }
@@ -60,35 +59,9 @@ class ComponentsSingle {
     public function metaboxes() {
         // Remove Yoast metabox
         remove_meta_box('wpseo_meta', get_current_screen(), 'normal');
-
-//        add_meta_box(
-//            'dot_layout_part_settings',
-//            __('Layout Part Settings', 'dotcore'),
-//            array($this, 'render_meta_box_main'),
-//            get_current_screen(),
-//            'normal',
-//            'high',
-//        );
-
     }
 
-    public
-    function render_meta_box_main() {
+    public function render_meta_box_main() {
 
-
-//        acf_render_field_wrap(array(
-//            'label'             => __('Field Group','acf'),
-//            'instructions'      => __('Choose a field group to assign to this component or <a href = "' . get_admin_url() . '/post-new.php?post_type=acf-field-group" > create a field group </a > ','dotcore'),
-//            'type'              => 'select',
-//            'name'              => 'dot_field_group',
-//            'prefix'            => 'acf_field_group',
-//            'value'             => (isset($group['dot_field_group'])) ? $group['dot_field_group'] : '',
-//            'toggle'            => false,
-//            'choices'           => $field_group_choices,
-//            'allow_null'        => true, // true | false
-//            'multiple'          => false, // true | false
-//            'ui'                => true, // true | false
-//            'ajax'              => false // true | false
-//        ));
     }
 }
