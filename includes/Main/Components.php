4c54f53041c8e2cf2ce6c809a33ce0ddf93fe7fb
Brieuc Fresnel <brieuc@studio-dot.fr>
2022-07-01 09:26:25 +0200

    Auto-indent Components.php

--- includes/Main/Components.php
+++ includes/Main/Components.php
@@ -3,267 +3,271 @@
 namespace DOT\Core\Main;
 
 class Components {
-    use HasTemplateFiles;
-    /**
-     * Post type slug
-     *
-     * @var string
-     */
-    public static string $post_type = 'dot-component';
+	use HasTemplateFiles;
 
-    public function __construct() {
-        // WP hooks
-        add_action('init', array($this, 'register_component'));
+	/**
+	 * Post type slug
+	 *
+	 * @var string
+	 */
+	public static string $post_type = 'dot-component';
 
-        // ACF hooks
-        add_filter('acf/location/rule_values/post_type', array($this, 'remove_component_from_post_types'));
-        add_filter('acf/location/rule_values/post', array($this, 'remove_component_from_posts'));
+	public function __construct() {
+		// WP hooks
+		add_action( 'init', array( $this, 'register_component' ) );
 
-        // ACF hooks - Component location rule
-        add_filter('acf/location/rule_types', array($this, 'location_types'));
-        add_filter('acf/location/rule_match/' . self::$post_type, array($this, 'location_match'), 10, 3);
-        add_filter('acf/location/rule_values/' . self::$post_type, array($this, 'location_values'), 10, 2);
-    }
+		// ACF hooks
+		add_filter( 'acf/location/rule_values/post_type', array( $this, 'remove_component_from_post_types' ) );
+		add_filter( 'acf/location/rule_values/post', array( $this, 'remove_component_from_posts' ) );
 
-    /**
-     * Get available components
-     *
-     * @return array
-     */
-    public function get_components(): array {
-        $query = new \WP_Query(array(
-            'post_type' => self::$post_type
-        ));
-        return $query->get_posts();
-    }
+		// ACF hooks - Component location rule
+		add_filter( 'acf/location/rule_types', array( $this, 'location_types' ) );
+		add_filter( 'acf/location/rule_match/' . self::$post_type, array( $this, 'location_match' ), 10, 3 );
+		add_filter( 'acf/location/rule_values/' . self::$post_type, array( $this, 'location_values' ), 10, 2 );
+	}
 
-    /**
-     * Render component template
-     *
-     * @param $component_id
-     * @return void
-     */
-    public function the_component($component_id) {
-        global $is_preview;
+	/**
+	 * Get available components
+	 *
+	 * @return array
+	 */
+	public function get_components(): array {
+		$query = new \WP_Query( array(
+			'post_type' => self::$post_type
+		) );
 
-        if (!is_numeric($component_id))
-            return;
+		return $query->get_posts();
+	}
 
-        $component = get_post($component_id);
-        if (!$component)
-            return;
+	/**
+	 * Render component template
+	 *
+	 * @param $component_id
+	 *
+	 * @return void
+	 */
+	public function the_component( $component_id ) {
+		if ( ! is_numeric( $component_id ) ) {
+			return;
+		}
 
-        $slug = $component->post_name;
+		$component = get_post( $component_id );
+		if ( ! $component ) {
+			return;
+		}
 
-
-        // Enqueue styles and script
-        $this->enqueue($slug, 'component');
-
-        // Render template
-        $this->render($slug, 'component');
-    }
+		$slug = $component->post_name;
 
 
-    /**
-     * Get field group attached to a component
-     *
-     * @param int $component_id
-     * @return false|array
-     */
-    public function get_field_group(int $component_id) {
-        $field_groups = acf_get_field_groups();
+		// Enqueue styles and script
+		$this->enqueue( $slug, 'component' );
 
-        foreach ($field_groups as $field_group) {
-            // Get first location rule only
-            // TODO : get_field_group_by_location : support multiple locations
-            $location = $field_group['location'][0][0];
+		// Render template
+		$this->render( $slug, 'component' );
+	}
 
-            if (
-                $location['param'] === \DOT\Core\Components::$post_type &&
-                $location['operator'] === '==' &&
-                $location['value'] == $component_id
-            ) {
-                return $field_group;
-            }
-        }
 
-        return false;
-    }
+	/**
+	 * Get field group attached to a component
+	 *
+	 * @param int $component_id
+	 *
+	 * @return false|array
+	 */
+	public function get_field_group( int $component_id ) {
+		$field_groups = acf_get_field_groups();
 
-    /**
-     * Remove Component from post types list
-     *
-     * @param $choices
-     *
-     * @return mixed
-     */
-    public function remove_component_from_post_types($choices) {
+		foreach ( $field_groups as $field_group ) {
+			// Get first location rule only
+			// TODO : get_field_group_by_location : support multiple locations
+			$location = $field_group['location'][0][0];
 
-        // Remove component
-        unset($choices[self::$post_type]);
+			if (
+				$location['param'] === \DOT\Core\Components::$post_type &&
+				$location['operator'] === '==' &&
+				$location['value'] == $component_id
+			) {
+				return $field_group;
+			}
+		}
 
-        return $choices;
-    }
+		return false;
+	}
 
-    /**
-     * Remove Components from posts list
-     *
-     * @param $choices
-     *
-     * @return mixed
-     */
-    public function remove_component_from_posts($choices) {
+	/**
+	 * Remove Component from post types list
+	 *
+	 * @param $choices
+	 *
+	 * @return mixed
+	 */
+	public function remove_component_from_post_types( $choices ) {
 
-        // Get post type labels
-        $post_type = get_post_type_labels(get_post_type_object(self::$post_type));
+		// Remove component
+		unset( $choices[ self::$post_type ] );
 
-        // Remove components
-        unset($choices[$post_type->singular_name]);
+		return $choices;
+	}
 
-        return $choices;
-    }
+	/**
+	 * Remove Components from posts list
+	 *
+	 * @param $choices
+	 *
+	 * @return mixed
+	 */
+	public function remove_component_from_posts( $choices ) {
 
-    /**
-     * Add component rule
-     *
-     * @param $choices
-     *
-     * @return mixed
-     */
-    public function location_types($choices) {
+		// Get post type labels
+		$post_type = get_post_type_labels( get_post_type_object( self::$post_type ) );
 
-        // Get post type labels
-        $post_type = get_post_type_labels(get_post_type_object(self::$post_type));
+		// Remove components
+		unset( $choices[ $post_type->singular_name ] );
 
-        // Add component option
-        $choices["DOT"][self::$post_type] = $post_type->singular_name;
+		return $choices;
+	}
 
-        return $choices;
-    }
+	/**
+	 * Add component rule
+	 *
+	 * @param $choices
+	 *
+	 * @return mixed
+	 */
+	public function location_types( $choices ) {
 
-    /**
-     * Component rule values
-     *
-     * @param $values
-     * @param $rule
-     *
-     * @return array
-     */
-    public function location_values($values, $rule) {
+		// Get post type labels
+		$post_type = get_post_type_labels( get_post_type_object( self::$post_type ) );
 
-        // Get posts grouped by
-        $posts = get_posts(
-            array(
-                'post_type' => self::$post_type,
-                'posts_per_page' => -1,
-            )
-        );
+		// Add component option
+		$choices["DOT"][ self::$post_type ] = $post_type->singular_name;
 
-        // Add "all" option
-        $values = array(
-            'all' => __('All', 'acf'),
-        );
+		return $choices;
+	}
 
-        // Build choices array
-        if (!empty($posts)) {
-            // Add posts
-            foreach ($posts as $post) {
-                $values[$post->ID] = $post->post_title;
-            }
-        }
+	/**
+	 * Component rule values
+	 *
+	 * @param $values
+	 * @param $rule
+	 *
+	 * @return array
+	 */
+	public function location_values( $values, $rule ) {
 
-        return $values;
-    }
+		// Get posts grouped by
+		$posts = get_posts(
+			array(
+				'post_type'      => self::$post_type,
+				'posts_per_page' => - 1,
+			)
+		);
 
-    /**
-     * Component rule matches
-     *
-     * @param $result
-     * @param $rule
-     * @param $screen
-     *
-     * @return bool
-     */
-    public function location_match($result, $rule, $screen) {
-        global $current_screen;
+		// Add "all" option
+		$values = array(
+			'all' => __( 'All', 'acf' ),
+		);
 
-        // Get post ID
-        $post_id = acf_maybe_get($screen, 'post_id');
+		// Build choices array
+		if ( ! empty( $posts ) ) {
+			// Add posts
+			foreach ( $posts as $post ) {
+				$values[ $post->ID ] = $post->post_title;
+			}
+		}
 
-        // If no post, return
-        if (!$post_id) {
-            return false;
-        }
+		return $values;
+	}
 
-        if ($rule['value'] === 'all') {
-            // Allow "all" to match any value.
-            $match = $current_screen->post_type === self::$post_type;
+	/**
+	 * Component rule matches
+	 *
+	 * @param $result
+	 * @param $rule
+	 * @param $screen
+	 *
+	 * @return bool
+	 */
+	public function location_match( $result, $rule, $screen ) {
+		global $current_screen;
 
-        } else {
-            // Compare all other values.
-            $match = (int)$post_id === (int)$rule['value'];
-        }
+		// Get post ID
+		$post_id = acf_maybe_get( $screen, 'post_id' );
 
-        // Allow for "!=" operator.
-        if ($rule['operator'] === '!=') {
-            $match = !$match;
-        }
+		// If no post, return
+		if ( ! $post_id ) {
+			return false;
+		}
 
-        // Contains operator
-        if ($rule['operator'] === 'contains') {
+		if ( $rule['value'] === 'all' ) {
+			// Allow "all" to match any value.
+			$match = $current_screen->post_type === self::$post_type;
 
-            $post_name = get_post_field('post_title', $post_id);
-            if (!$post_name) {
-                return false;
-            }
+		} else {
+			// Compare all other values.
+			$match = (int) $post_id === (int) $rule['value'];
+		}
 
-            // Compare
-            return (stripos($post_name, $rule['value']) !== false);
-        }
+		// Allow for "!=" operator.
+		if ( $rule['operator'] === '!=' ) {
+			$match = ! $match;
+		}
 
-        return $match;
-    }
+		// Contains operator
+		if ( $rule['operator'] === 'contains' ) {
 
-    public function register_component() {
-        $labels = array(
-            'name' => _x('Components', 'Post type general name', 'dotcore'),
-            'singular_name' => _x('Component', 'Post type singular name', 'dotcore'),
-            'menu_name' => _x('Components', 'Admin Menu text', 'dotcore'),
-            'name_admin_bar' => _x('Component', 'Add New on Toolbar', 'dotcore'),
-            'add_new' => __('Add New', 'dotcore'),
-            'add_new_item' => __('Add New Component', 'dotcore'),
-            'new_item' => __('New Component', 'dotcore'),
-            'edit_item' => __('Edit Component', 'dotcore'),
-            'view_item' => __('View Component', 'dotcore'),
-            'all_items' => __('All Components', 'dotcore'),
-            'search_items' => __('Search Components', 'dotcore'),
-            'parent_item_colon' => __('Parent Components:', 'dotcore'),
-            'not_found' => __('No components found.', 'dotcore'),
-            'not_found_in_trash' => __('No components found in Trash.', 'dotcore'),
-            'featured_image' => _x('Component Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'dotcore'),
-            'set_featured_image' => _x('Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'dotcore'),
-            'remove_featured_image' => _x('Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'dotcore'),
-            'use_featured_image' => _x('Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'dotcore'),
-            'archives' => _x('Component archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'dotcore'),
-            'insert_into_item' => _x('Insert into Component', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'dotcore'),
-            'uploaded_to_this_item' => _x('Uploaded to this Component', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'dotcore'),
-            'filter_items_list' => _x('Filter Components list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'dotcore'),
-            'items_list_navigation' => _x('Components list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'dotcore'),
-            'items_list' => _x('Components list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'dotcore'),
-        );
+			$post_name = get_post_field( 'post_title', $post_id );
+			if ( ! $post_name ) {
+				return false;
+			}
 
-        $args = array(
-            'labels' => $labels,
-            'public' => true,
-            'publicly_queryable' => true,
-            'show_in_menu' => false,
-            'query_var' => true,
-            'rewrite' => array('slug' => self::$post_type),
-            'capability_type' => 'post',
-            'hierarchical' => false,
-            'supports' => array('title'),
-        );
+			// Compare
+			return ( stripos( $post_name, $rule['value'] ) !== false );
+		}
 
-        register_post_type(self::$post_type, $args);
-    }
+		return $match;
+	}
+
+	public function register_component() {
+		$labels = array(
+			'name'                  => _x( 'Components', 'Post type general name', 'dotcore' ),
+			'singular_name'         => _x( 'Component', 'Post type singular name', 'dotcore' ),
+			'menu_name'             => _x( 'Components', 'Admin Menu text', 'dotcore' ),
+			'name_admin_bar'        => _x( 'Component', 'Add New on Toolbar', 'dotcore' ),
+			'add_new'               => __( 'Add New', 'dotcore' ),
+			'add_new_item'          => __( 'Add New Component', 'dotcore' ),
+			'new_item'              => __( 'New Component', 'dotcore' ),
+			'edit_item'             => __( 'Edit Component', 'dotcore' ),
+			'view_item'             => __( 'View Component', 'dotcore' ),
+			'all_items'             => __( 'All Components', 'dotcore' ),
+			'search_items'          => __( 'Search Components', 'dotcore' ),
+			'parent_item_colon'     => __( 'Parent Components:', 'dotcore' ),
+			'not_found'             => __( 'No components found.', 'dotcore' ),
+			'not_found_in_trash'    => __( 'No components found in Trash.', 'dotcore' ),
+			'featured_image'        => _x( 'Component Cover Image', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'dotcore' ),
+			'set_featured_image'    => _x( 'Set cover image', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'dotcore' ),
+			'remove_featured_image' => _x( 'Remove cover image', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'dotcore' ),
+			'use_featured_image'    => _x( 'Use as cover image', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'dotcore' ),
+			'archives'              => _x( 'Component archives', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'dotcore' ),
+			'insert_into_item'      => _x( 'Insert into Component', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'dotcore' ),
+			'uploaded_to_this_item' => _x( 'Uploaded to this Component', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'dotcore' ),
+			'filter_items_list'     => _x( 'Filter Components list', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'dotcore' ),
+			'items_list_navigation' => _x( 'Components list navigation', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'dotcore' ),
+			'items_list'            => _x( 'Components list', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'dotcore' ),
+		);
+
+		$args = array(
+			'labels'             => $labels,
+			'public'             => true,
+			'publicly_queryable' => true,
+			'show_in_menu'       => false,
+			'query_var'          => true,
+			'rewrite'            => array( 'slug' => self::$post_type ),
+			'capability_type'    => 'post',
+			'hierarchical'       => false,
+			'supports'           => array( 'title' ),
+		);
+
+		register_post_type( self::$post_type, $args );
+	}
 }
